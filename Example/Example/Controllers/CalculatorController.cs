fbf2e66479b213bf01f4c8d128016cce25013608
metel68 <maxmetel@gmail.com>
2015-07-09 15:40:09 +0600

    Added Options

--- Example/Example/Controllers/CalculatorController.cs
+++ Example/Example/Controllers/CalculatorController.cs
@@ -15,6 +15,18 @@ namespace Example.Controllers
             ViewBag.operations = new List<object>
                                      {
                                          new { value = "Сложение", calculatorName = "Add", operationType = "twoArguments" },
+                                         new { value = "Вычитание", calculatorName = "Sub", operationType = "twoArguments" },
+                                         new { value = "Умножение", calculatorName = "Mult", operationType = "twoArguments" },
+                                         new { value = "Деление", calculatorName = "Div", operationType = "twoArguments" },
+                                         new { value = "Степерь", calculatorName = "Pow", operationType = "twoArguments" },
+                                         new { value = "Корень по любому основанию", calculatorName = "Root", operationType = "twoArguments" },
+                                         new { value = "Модуль", calculatorName = "Abs", operationType = "oneArgument" },
+                                         new { value = "Синус", calculatorName = "Sin", operationType = "oneArgument" },
+                                         new { value = "Косинус", calculatorName = "Cos", operationType = "oneArgument" },
+                                         new { value = "Тангенс", calculatorName = "Tan", operationType = "oneArgument" },
+                                         new { value = "Арксинус", calculatorName = "Arcsin", operationType = "oneArgument" },
+                                         new { value = "Арккосинус", calculatorName = "Arccos", operationType = "oneArgument" },
+                                         new { value = "Арктангенс", calculatorName = "Arctan", operationType = "oneArgument" },
                                          new { value = "Извлечение квадратного корня", calculatorName = "Sqrt", operationType = "oneArgument" }
                                      };
             return View();
